05b76494465e2b83bc70f3de00b1766158aa6eda
Dovydas Venckus <dovydas.venckus@live.com>
2017-08-19 23:57:26 +0300

    Added example of jersey on embedded jetty

---
+++ src/main/java/com/dovydasvenckus/jersey/JerseyApplication.java
@@ -0,0 +1,44 @@
+package com.dovydasvenckus.jersey;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.eclipse.jetty.servlet.ServletContextHandler.NO_SESSIONS;
+
+public class JerseyApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(JerseyApplication.class);
+
+    public static void main(String[] args) {
+
+        Server server = new Server(8080);
+
+        ServletContextHandler servletContextHandler = new ServletContextHandler(NO_SESSIONS);
+
+        servletContextHandler.setContextPath("/");
+        server.setHandler(servletContextHandler);
+
+        ServletHolder servletHolder = servletContextHandler.addServlet(ServletContainer.class, "/api/*");
+        servletHolder.setInitOrder(0);
+        servletHolder.setInitParameter(
+                "jersey.config.server.provider.packages",
+                "com.dovydasvenckus.jersey.resources"
+        );
+
+        try {
+            server.start();
+            server.join();
+        } catch (Exception ex) {
+            logger.error("Error occurred while starting Jetty", ex);
+            System.exit(1);
+        }
+
+        finally {
+            server.destroy();
+        }
+    }
+}
---
+++ src/main/java/com/dovydasvenckus/jersey/resources/HelloResource.java
@@ -0,0 +1,18 @@
+package com.dovydasvenckus.jersey.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloResource {
+
+    @GET
+    @Path("/{param}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMessage(@PathParam("param") String msg) {
+        return "Hello " + msg;
+    }
+}
